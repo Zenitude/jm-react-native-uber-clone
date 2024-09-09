34a3ae03ff3fd00ec1c6260f116045501a4cb85f
Rudy Mollet <mollet.rudy@gmail.com>
2024-09-09 18:19:38 +0200

    define hook userDriverStore

--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,4 @@
-import { LocationStore } from "@/types/global"
+import { DriverStore, LocationStore, MarkerData } from "@/types/global"
 import { create } from "zustand"
 
 export const useLocationStore = create<LocationStore>((set) => ({
@@ -36,4 +36,13 @@ export const useLocationStore = create<LocationStore>((set) => ({
 			destinationLongitude: longitude,
 			destinationAddress: address,
 		})),
+}))
+
+export const useDriverStore = create<DriverStore>((set) => ({
+	drivers: [] as MarkerData[],
+	selectedDriver: null,
+	setSelectedDriver: (driverId: number) =>
+		set(() => ({ selectedDriver: driverId })),
+	setDrivers: (drivers: MarkerData[]) => set(() => ({ drivers: drivers })),
+	clearSelectedDriver: () => set(() => ({ selectedDriver: null })),
 }))
