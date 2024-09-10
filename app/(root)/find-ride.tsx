3cfb133108e96cb9114599855d4d9320e355731c
Rudy Mollet <mollet.rudy@gmail.com>
2024-09-10 17:23:33 +0200

    prepare ride layout

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,10 +1,24 @@
 import { View, Text } from "react-native"
 import React from "react"
+import { useLocationStore } from "@/store"
 
 export default function FindRide() {
+	const {
+		userAddress,
+		destinationAddress,
+		setDestinationLocation,
+		setUserLocation,
+	} = useLocationStore()
 	return (
 		<View>
-			<Text>FindRide</Text>
+			<Text className={styles.title}>You are here : {userAddress}</Text>
+			<Text className={styles.title}>
+				You are going to : {destinationAddress}
+			</Text>
 		</View>
 	)
+}
+
+const styles = {
+	title: "text-2xl",
 }
